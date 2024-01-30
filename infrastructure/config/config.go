e34ea5211b2107bf300756392873f17a4c61234e
romxxxx <romuald.brune@gmail.com>
2024-01-30 02:18:37 +0100

    Version 1.0.1

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -34,7 +34,7 @@ const (
 	defaultLogFilename         = "nexelliad.log"
 	defaultErrLogFilename      = "nexelliad_err.log"
 	defaultTargetOutboundPeers = 8
-	defaultMaxInboundPeers     = 157
+	defaultMaxInboundPeers     = 117
 	defaultBanDuration         = time.Hour * 24
 	defaultBanThreshold        = 100
 	//DefaultConnectTimeout is the default connection timeout when dialing
